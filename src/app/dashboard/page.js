24ff1ac1ad3bc713a4ab03746fbe672613bfd96a
lernEarnBurn <lerners124@gmail.com>
2024-03-06 15:55:02 +0200

    set up a basic structure for project

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,9 @@
+
+export default function Dashboard() {
+  return (
+      <div style={{ padding: "20px", textAlign: "center" }}>
+        <h1 className="bg-black">Welcome to the Stock Watchlist App</h1>
+        <p>Your one-stop destination to track your favorite stocks.</p>
+      </div>
+  );
+}
