a994418bf351780eeb2cfa2f0f33d575d95b5e41
lernEarnBurn <lerners124@gmail.com>
2024-03-17 22:47:10 +0200

    added user model for db

---
+++ src/utils/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+  watchlist: {
+    type: [String],
+    default: ['SPY', 'QQQ', 'DIA', 'TSLA', 'AAPL', 'AMZN', 'WMT']
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
