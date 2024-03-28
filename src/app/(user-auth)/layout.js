02c147a16cc4eede04067b177f088043f7f7e05c
lernEarnBurn <lerners124@gmail.com>
2024-03-28 16:03:08 +0200

    some frontend implementation of auth forms

--- src/app/(user-auth)/layout.js
+++ src/app/(user-auth)/layout.js
@@ -2,6 +2,7 @@ import { WavyBackground } from "@/components/wavy-background";
 
 export default function Layout({children}) {
   return (
+      //attempt to figure out how this works
       <WavyBackground>
         <main>{children}</main>
       </WavyBackground>
--- src/app/(user-auth)/signup/page.js
+++ src/app/(user-auth)/signup/page.js
@@ -2,6 +2,8 @@
 
 import { motion } from 'framer-motion'
 import Link from 'next/link';
+import { useState } from 'react';
+import axios from 'axios';
 
 
 export default function Signup(){
@@ -10,17 +12,44 @@ export default function Signup(){
     hover: { scale: 1.02 },
   };
 
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value)
+  }
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value)
+  }
+
+  async function sendSignup(){
+    if(username !== '' && password !== ''){
+      try{
+        const response = await axios.post('api/auth/signup', {username, password})
+        console.log(response.data)
+        setUsername('')
+        setPassword('')
+      } catch(err){
+        console.log(err)
+      }
+    }
+  }
+
+
+
   return (
     <>
       <div className="flex flex-col gap-5 justify-center items-center h-[80vh]">
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
+        <input onChange={handleUsernameChange} type="text" placeholder="username"/>
+        <input onChange={handlePasswordChange} type="password" placeholder="password"/>
         <motion.button
           className="mt-2"
           variants={buttonVariants}
           whileHover="hover"
           whileTap="rest"
           initial="rest"
+          onClick={sendSignup}
          >Submit
         </motion.button>
         <Link className='self-end underline text-sm' href="/login">Already Have Account</Link>
