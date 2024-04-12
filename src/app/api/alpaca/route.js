3fa73686eadb4869afc8f053d3a2930abe78e01b
onetimecircle <lerners124@gmail.com>
2024-04-12 11:15:53 -0400

    route wil get the stock prices

--- src/app/api/alpaca/route.js
+++ src/app/api/alpaca/route.js
@@ -1,73 +1,28 @@
+import { NextResponse } from 'next/server';
 const Alpaca = require('@alpacahq/alpaca-trade-api');
 
-let alpaca;
-let socket;
-let clients = [];
-
 export async function POST(req) {
-  const { action, symbols } = await req.json();
+  const { symbols } = await req.json();
+  
+  try {
+    const alpaca = new Alpaca( {baseUrl: 'https://paper-api.alpaca.markets/',
+                                keyId: "PKCF91C8L1Y9ZNI3XNLK",
+                                secretKey: "eDagbliCZ9PBSeSRKZVChGMFYuAbVSAMzos0RyHH",});
 
-  if (action === "connect") {
-    alpaca = new Alpaca({
-      keyId: process.env.ALPACA_API_KEY,
-      secretKey: process.env.ALPACA_SECRET,
-      feed: "sip",
-      paper: true,
-    });
+    
+  
+      
+    const quotes = await alpaca.getLatestQuotes(symbols);
+    
+    const quoteData = {};
+    for (const [symbol, quote] of quotes.entries()) {
+      quoteData[symbol] = quote.AskPrice;
+    }
 
-    socket = alpaca.data_stream_v2;
 
-    socket.onConnect(function () {
-      console.log("Connected");
-      socket.subscribeForQuotes(symbols);
-    });
-
-    socket.onError((err) => {
-      console.log(err);
-    });
-
-    socket.onStockQuote((quote) => {
-      console.log("Quote:", quote);
-      clients.forEach((client) => {
-        client.write(`data: ${JSON.stringify(quote)}\n\n`);
-      });
-    });
-
-    socket.onStateChange((state) => {
-      console.log("State:", state);
-    });
-
-    socket.onDisconnect(() => {
-      console.log("Disconnected");
-    });
-
-    socket.connect();
-
-    const { readable, writable } = new TransformStream();
-    clients.push(writable);
-
-    return new Response(readable, {
-      headers: {
-        'Content-Type': 'text/event-stream',
-        Connection: 'keep-alive',
-        'Cache-Control': 'no-cache',
-      },
-    });
-  } else if (action === "disconnect") {
-    socket.disconnect();
-
-    return new Response(JSON.stringify({ message: "WebSocket disconnected" }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } else {
-    return new Response(JSON.stringify({ message: "Invalid action" }), {
-      status: 400,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+    return NextResponse.json(quoteData);
+  } catch (error) {
+    console.error('Error fetching quotes:', error);
+    return NextResponse.json({ error: 'Failed to fetch quotes' }, { status: 500 });
   }
 }
