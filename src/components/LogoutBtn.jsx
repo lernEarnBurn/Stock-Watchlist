146c4fdac7b979afc3844b65e53fdfb400325da5
onetimecircle <lerners124@gmail.com>
2024-04-16 11:08:22 -0400

    styled logout button

--- src/components/LogoutBtn.jsx
+++ src/components/LogoutBtn.jsx
@@ -1,11 +1,16 @@
 'use client';
 
+import { LogOut, Loader2 } from 'lucide-react';
 import { useRouter } from "next/navigation";
+import { useState } from 'react';
 
 export default function LogoutBtn() {
   const router = useRouter();
 
+  const [loading, setLoading] = useState(false)
+
   async function handleLogout() {
+    setLoading(true)
     const response = await fetch('/api/auth/logout', {
       method: 'POST',
       headers: {
@@ -14,15 +19,24 @@ export default function LogoutBtn() {
     });
 
     if (response.ok) {
+      setLoading(false)
       router.push('/login');
     } else {
       console.log('logout failed');
     }
   }
-
   return (
-    <button className="button" onClick={handleLogout}>
-      Logout
-    </button>
-  );
+    <>
+    {!loading ? (
+        <button className="logout-button" onClick={handleLogout}>
+          <LogOut/>
+        </button>
+      ) : (
+        <button disabled className="logout-button">
+          <Loader2 className="animate-spin"/>
+        </button>
+      )
+    }
+    </>
+  )
 }
